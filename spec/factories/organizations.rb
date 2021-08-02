4031717daee071f86b02909492a681b28bc277e8
Guillermo Alvarez <guillermo@icalialabs.com>
2021-08-02 12:14:12 -0600

    Annotates models

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: organizations
+#
+#  id              :bigint           not null, primary key
+#  card_number     :integer
+#  cvv             :integer
+#  expiration_date :date
+#  name            :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  plan_id         :bigint           not null
+#  user_id         :bigint           not null
+#
+# Indexes
+#
+#  index_organizations_on_plan_id  (plan_id)
+#  index_organizations_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (plan_id => plans.id)
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :organization do
     name { "MyString" }
--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: plans
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :plan do
     name { "MyString" }
