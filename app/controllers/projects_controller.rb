f0611f61929adb2998aaee519dd6bb5ae97355b8
Guillermo Alvarez <guillermo@icalialabs.com>
2021-08-09 12:39:37 -0600

    Adds logic to handle a project creation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,21 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @project = current_user.organization.projects.build(project_params)
+
+    if @project.save
+      #TODO: redirect to organizations home
+      render :home, notice: 'The project was created successfully'
+    else
+      render :new, alert: 'The project could not be saved'
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :details, :expected_completion)
+  end
 end
