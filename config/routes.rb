85f8a7c5bcab3f4aec7618221633cedd6d628e66
Guillermo Alvarez <guillermo@icalialabs.com>
2021-08-09 11:52:46 -0600

    Adds new project form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  
+
   root to: 'home#show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :projects, only: %i[new create]
 end
